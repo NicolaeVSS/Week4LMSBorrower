04fb6607207197d16fea3c9f47e4a01fb39b9395
AbdiN7 <55746065+AbdiN7@users.noreply.github.com>
2019-10-31 17:42:24 -0400

    test 4

--- src/main/java/com/ss/lms/LmsWk3Application.java
+++ src/main/java/com/ss/lms/LmsWk3Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class LmsWk3Application {
-	//testing webhook #3
+	//testing webhook #4
 	public static void main(String[] args) {
 		SpringApplication.run(LmsWk3Application.class, args);
 		
